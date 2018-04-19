3f7e1a7e4cb18dac9d29a02ca3fba48cbe0e638c
Terry Allitt <tallitt88@gmail.com>
2018-04-19 10:49:37 +0100

    :children_crossing: Build code in Takeaway.rb for user interaction when trying to view menu - now to create Menu.rb

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,6 +1,8 @@
 class Takeaway
   def launch!
     introduction
+    home_options
+    home_page(STDIN.gets.chomp.strip.downcase.to_sym)
     # action loop
       # what do you want to do?
       # do that action
@@ -8,8 +10,26 @@ class Takeaway
     conclusion
   end
 
+  def home_options
+    puts "Select an option:"
+    puts "Type 'Menu' to View Menu"
+  end
+
+  def home_page(selection)
+    case selection
+    when :menu then display_menu
+    end
+  end
+
+  private
+
   def introduction
     puts "Welcome to Takeaway Express"
+  end
+
+  def display_menu
+    @menu.print
+    home_options
   end
 
   def conclusion
