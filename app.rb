fc42fdf3f85b6ea9f41db0b1ee2c84f37c995cec
Terry Allitt <tallitt88@gmail.com>
2018-04-19 10:24:57 +0100

    :construction: Add method to Takeaway.rb to launch application - method currently has an intro and conclusion for the user

--- app.rb
+++ app.rb
@@ -2,3 +2,6 @@ APP_ROOT = File.dirname(__FILE__)
 # Special variable containing array of all folders Ruby will look in
 $:.unshift( File.join(APP_ROOT, 'lib') )
 require 'takeaway'
+
+takeaway = Takeaway.new
+takeaway.launch!
--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,1 +1,18 @@
-puts "Hello World"
+class Takeaway
+  def launch!
+    introduction
+    # action loop
+      # what do you want to do?
+      # do that action
+    # repeat until user quits
+    conclusion
+  end
+
+  def introduction
+    puts "Welcome to Takeaway Express"
+  end
+
+  def conclusion
+    puts "Goodbye and happy munching!"
+  end
+end
